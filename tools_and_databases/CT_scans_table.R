33af9c625ef2bdf3dab5750b985786c7728f8bec
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2021-11-19 14:29:14 +0100

    moved radiomics part to another repo

--- tools_and_databases/CT_scans_table.R
+++
@@ -1,7 +0,0 @@
-library(reactable)
-
-tabla_databases <- read.csv(file = "C:/Iker/ISGlobal/DataSHIELD_utils/tools_and_databases/CT_scans_table.csv", header=TRUE, sep = "\t")
-
-tabla_databases
-
-reactable
